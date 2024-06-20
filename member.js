a895f6ba45c8eb73f9eddb74b8e111300b3488a5
mrth90 <mrth683du@gmail.com>
2024-06-20 00:01:46 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    // The member is an object with the following properties
+    var member = {
+        name: "John Doe",
+        skills: ["JavaScript", "HTML", "CSS"]
+    };
+    return member;
+}
